a774204b9cc8e118a0bb6b4d0a66897195ea42e6
LijTesfaye <mesay1964@gmial.com>
2023-06-09 11:03:27 -0400

    MainClass class created

---
+++ src/main/java/it/unipi/cc/toretto/kmeans/KMeansMRMain.java
@@ -0,0 +1,8 @@
+package it.unipi.cc.toretto.kmeans;
+
+public class KMeansMRMain {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Kmeans from Main");
+        System.exit(0);
+    }
+}
---
+++ src/test/java/it/unipi/cc/toretto/kmeans/DataModelTest.java
@@ -0,0 +1,9 @@
+package it.unipi.cc.toretto.kmeans;
+import junit.framework.TestCase;
+public class DataModelTest  extends TestCase {
+    // TODO case_1: the Euclidean distance test
+
+    // TODO case_2: the Manhattan distance test
+
+    // TODO case_3: if there are other distance test cases
+}
